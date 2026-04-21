f6798dc57a2f0372f01104983c2ed64fd24cc639
sneh <strivedi@zscaler.com>
2026-04-20 20:33:09 -0700

    chg: add iperf server in dedicated VPC for base_cc_asg
    Create a separate VPC with subnet, router, NAT gateway, and firewall
    rules for an iperf3 server with a public IP. The server auto-installs
    iperf3 on startup and listens on port 5201. Connection details are
    included in testbed.txt and ssh_config outputs.

--- examples/base_cc_asg/main.tf
+++ examples/base_cc_asg/main.tf
@@ -116,6 +116,94 @@ resource "google_compute_route" "route_to_cc_vm" {
 
 
 ################################################################################
+# 3b. Create dedicated VPC, subnet, and iperf server with public IP
+################################################################################
+resource "google_compute_network" "iperf_vpc" {
+  name                    = "${var.name_prefix}-iperf-vpc-${random_string.suffix.result}"
+  auto_create_subnetworks = false
+  project                 = coalesce(var.project_host, var.project)
+}
+
+resource "google_compute_subnetwork" "iperf_subnet" {
+  name          = "${var.name_prefix}-iperf-subnet-${random_string.suffix.result}"
+  ip_cidr_range = "10.1.225.0/24"
+  network       = google_compute_network.iperf_vpc.self_link
+  region        = var.region
+}
+
+resource "google_compute_router" "iperf_router" {
+  name    = "${var.name_prefix}-iperf-router-${random_string.suffix.result}"
+  network = google_compute_network.iperf_vpc.self_link
+}
+
+resource "google_compute_router_nat" "iperf_nat" {
+  name                               = "${var.name_prefix}-iperf-nat-${random_string.suffix.result}"
+  router                             = google_compute_router.iperf_router.name
+  region                             = google_compute_router.iperf_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+resource "google_service_account" "iperf_service_account" {
+  account_id   = "${var.name_prefix}-iperf-sa-${random_string.suffix.result}"
+  display_name = "${var.name_prefix}-iperf-sa-${random_string.suffix.result}"
+}
+
+resource "google_compute_instance" "iperf_server" {
+  name         = "${var.name_prefix}-iperf-server-${random_string.suffix.result}"
+  machine_type = "e2-medium"
+  zone         = length(var.zones) == 0 ? data.google_compute_zones.available.names[0] : var.zones[0]
+  tags         = ["zscc-iperf"]
+  network_interface {
+    subnetwork = google_compute_subnetwork.iperf_subnet.self_link
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${tls_private_key.key.public_key_openssh}"
+  }
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y iperf3 net-tools
+    systemctl enable --now iperf3 || iperf3 -s -D
+  EOT
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      type  = "pd-ssd"
+      size  = "10"
+    }
+  }
+  service_account {
+    email  = google_service_account.iperf_service_account.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_firewall" "iperf_ingress" {
+  name        = "${var.name_prefix}-fw-iperf-ingress-${random_string.suffix.result}"
+  description = "Permit iperf3 (TCP/UDP 5201) and SSH to iperf server"
+  network     = google_compute_network.iperf_vpc.self_link
+  direction   = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["5201", "22"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["5201"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges           = ["0.0.0.0/0"]
+  target_service_accounts = [google_service_account.iperf_service_account.email]
+}
+
+
+################################################################################
 # 4. Create specified number CC VMs per cc_count which will span equally across 
 #    designated availability zones per az_count. E.g. cc_count set to 4 and 
 #    az_count set to 2 will create 2x CCs in AZ1 and 2x CCs in AZ2
